12fe5cc3f6889dd56ad817738a13a97282fddeb0
d3str0yer <56647723+d3str0yer@users.noreply.github.com>
2019-10-27 16:05:49 +0100

    Add files via upload

--- files/config.sh
+++ files/config.sh
@@ -58,18 +58,18 @@ sed -i "s/#ignoreip = 127.0.0.1\/8 ::1/ignoreip = 127.0.0.1\/8 ::1 10.0.0.0\/8 1
 sed -i "s/bantime  = 10m/bantime = 21600/" /etc/fail2ban/jail.local
 sed -i "s/findtime  = 10m/findtime = 21600/" /etc/fail2ban/jail.local
 sed -i "s/maxretry = 5/maxretry = 3/" /etc/fail2ban/jail.local
-/etc/init.d/fail2ban restart 2>&1 >> log.txt
+/etc/init.d/fail2ban restart
 
 #configuration of ssh
 echo "configuring ssh..."
 sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/" /etc/ssh/sshd_config
-service ssh restart 2>&1 >> log.txt
+service ssh restart
 
 #config swapfile
 echo "increasing swapfile..."
-dphys-swapfile swapoff 2>&1 >> log.txt
+dphys-swapfile swapoff
 sed -i "s/CONF_SWAPSIZE=100/CONF_SWAPSIZE=1000/" /etc/dphys-swapfile
-dphys-swapfile swapon 2>&1 >> log.txt
+dphys-swapfile swapon
 
 #create folders for mounting/keeping downloaded stuff
 echo "creating folders..."
@@ -77,7 +77,88 @@ mkdir -p /mnt/downloading
 mkdir -p /mnt/hdd/archived
 mkdir /mnt/hdd/torrentfiles
 mkdir /mnt/hdd/watching
+tree /mnt --noreport
 
+# qbittorrent stuff
+echo "creating user for qbittorrent..."
+useradd -p $(openssl passwd -1 supersecretpasswordforqbittorrent) -d /home/qbtuser -m -c "qbittorrent user" -s /usr/sbin/nologin qbtuser
+echo "[LegalNotice]" >> /home/qbtuser/.config/qBittorrent/qBittorrent.conf
+echo "Accepted=true" >> /home/qbtuser/.config/qBittorrent/qBittorrent.conf
+echo "setting up qbittorrent service..."
+echo "[Unit]" >> /etc/systemd/system/qbittorrent.service
+echo "Description=qBittorrent Daemon Service" >> /etc/systemd/system/qbittorrent.service
+echo "After=network.target" >> /etc/systemd/system/qbittorrent.service
+echo >> /etc/systemd/system/qbittorrent.service
+echo "[Service]" >> /etc/systemd/system/qbittorrent.service
+echo "User=qbtuser" >> /etc/systemd/system/qbittorrent.service
+echo "ExecStart=/usr/bin/qbittorrent-nox" >> /etc/systemd/system/qbittorrent.service
+echo "ExecStop=/usr/bin/killall -w qbittorrent-nox" >> /etc/systemd/system/qbittorrent.service
+echo >> /etc/systemd/system/qbittorrent.service
+echo "[Install]" >> /etc/systemd/system/qbittorrent.service
+echo "WantedBy=multi-user.target" >> /etc/systemd/system/qbittorrent.service
+#wHy CrEaTe A sErVicE iF YoU'rE GONnA DisAbLe It?????
+systemctl disable qbittorrent
+sudo chown -R qbtuser:qbtuser /mnt/
+
+echo "setting up cronjobs..."
+echo "0 */6 * * * sudo service openvpn restart >/dev/null 2>&1" >> cronjob
+echo "*/5 * * * * nice /home/pi/torrentbox-master/files/vnstati.sh >/dev/null 2>&1" >> cronjob
+crontab cronjob
+rm cronjob
+
+echo "set openvpn to autostart..."
+sed -i "s/#AUTOSTART=\"all\"/AUTOSTART=\"openvpn\"/" /etc/default/openvpn
+
+echo "setting up delay for qbittorrent..."
+sed -i "s/exit 0//" /etc/rc.local
+echo "sleep 7" >> /etc/rc.local #might need to be adapted depending on hdd speedup time
+echo "service qbittorrent start" >> /etc/rc.local
+echo "exit 0" >> /etc/rc.local
+
+echo "setting up motd..."
+echo "/home/pi/torrentbox-master/files/welcome.sh" >> /etc/profile
+echo "" > /etc/motd
+echo "MOTD will display your storage space, for this it needs to know how many devices you have attached. (Select 1 or 2)"
+while read -r -n 1 -s answer; do
+  if [[ $answer = [12] ]]; then
+    [[ $answer = [1] ]] && cp files/welcome1.sh files/welcome.sh
+    [[ $answer = [2] ]] && cp files/welcome2.sh files/welcome.sh
+    break
+  fi
+done
+rm files/welcome[12].sh 
+chmod u+x files/welcome.sh
+echo
+
+echo "setting up smb server..."
+echo "[Torrents]" >> /etc/samba/smb.conf
+echo "Comment = Samba Share for Torrents" >> /etc/samba/smb.conf
+echo "Path = /mnt" >> /etc/samba/smb.conf
+echo "Browseable = yes" >> /etc/samba/smb.conf
+echo "Writeable = Yes" >> /etc/samba/smb.conf
+echo "read only = no" >> /etc/samba/smb.conf
+echo "only guest = no" >> /etc/samba/smb.conf
+echo "create mask = 0777" >> /etc/samba/smb.conf
+echo "directory mask = 0777" >> /etc/samba/smb.conf
+echo "Public = no" >> /etc/samba/smb.conf
+
+echo -e "${RED}Please set a Password for the Network Share:${DEF}"
+smbpasswd -a pi
+systemctl restart smbd
+chmod -R 757 /mnt
+
+echo "copy paste openvpn shit into /etc/openvpn"
+read -n 1 -s -r -p "Press any key to continue"
+openvpn /etc/openvpn/openvpn.conf &
+sleep 10
+echo "${RED}Your IP is `curl -s icanhazip.com`${DEF}"
+
+#iptables firewall config
+echo "installing iptables-persistent..."
+#to surpress the popup that the install will give
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
+apt-get install iptables-persistent -y
 #change sysctl.conf to disable ipv6
 echo "disabling ipv6..."
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
@@ -85,9 +166,7 @@ echo "net.ipv6.conf.all.disable_ipv6=1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.default.disable_ipv6=1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.lo.disable_ipv6=1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.eth0.disable_ipv6=1" >> /etc/sysctl.conf
-
-#iptables firewall config
-echo "ceating iptables..."
+#echo "ceating iptables..."
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 iptables -A INPUT -s 192.168.178.0/24 -j ACCEPT
@@ -103,100 +182,17 @@ iptables -A INPUT -i tun+ -j ACCEPT
 iptables -P INPUT DROP
 iptables -P OUTPUT DROP
 iptables -P FORWARD DROP
-# to surpress the popup that the install will give
-echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
-echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
-echo "installing iptables-persistent..."
-apt-get install iptables-persistent -y 2>&1 >> log.txt
-netfilter-persistent save
 echo "saving iptables..."
+netfilter-persistent save
 systemctl enable netfilter-persistent
 
-#create user for qbittorrent called qbtuser & setting up qbittorrent as a service
-echo "creating user for qbittorrent..."
-useradd -p $(openssl passwd -1 supersecretpasswordforqbittorrent) qbtuser
-echo "setting up qbittorrent service..."
-echo "[Unit]" >> /etc/systemd/system/qbittorrent.service
-echo "Description=qBittorrent Daemon Service" >> /etc/systemd/system/qbittorrent.service
-echo "After=network.target" >> /etc/systemd/system/qbittorrent.service
-echo >> /etc/systemd/system/qbittorrent.service
-echo "[Service]" >> /etc/systemd/system/qbittorrent.service
-echo "User=qbtuser" >> /etc/systemd/system/qbittorrent.service
-echo "ExecStart=/usr/bin/qbittorrent-nox" >> /etc/systemd/system/qbittorrent.service
-echo "ExecStop=/usr/bin/killall -w qbittorrent-nox" >> /etc/systemd/system/qbittorrent.service
-echo >> /etc/systemd/system/qbittorrent.service
-echo "[Install]" >> /etc/systemd/system/qbittorrent.service
-echo "WantedBy=multi-user.target" >> /etc/systemd/system/qbittorrent.service
-
-
-echo -e "${RED}########################################################${DEF}"
-echo -e "${RED}Please manually accept this prompt and then press CTRL+C${DEF}"
-echo -e "${RED}########################################################${DEF}"
-su -c "qbittorrent-nox" - qbtuser
-
-usermod -s /usr/sbin/nologin qbtuser
-chown -R qbtuser:qbtuser /mnt/
-systemctl disable qbittorrent
-
-echo "setting up smb server..."
-echo "[Torrents]" >> /etc/samba/smb.conf
-echo "Comment = Samba Share for Torrents" >> /etc/samba/smb.conf
-echo "Path = /mnt" >> /etc/samba/smb.conf
-echo "Browseable = yes" >> /etc/samba/smb.conf
-echo "Writeable = Yes" >> /etc/samba/smb.conf
-echo "only guest = no" >> /etc/samba/smb.conf
-echo "create mask = 0777" >> /etc/samba/smb.conf
-echo "directory mask = 0777" >> /etc/samba/smb.conf
-echo "Public = yes" >> /etc/samba/smb.conf
-
-echo -e "${RED}Please set a Password for the Network Share:${DEF}"
-smbpasswd -a pi
-systemctl restart smbd
-
-echo "creating vnstati script..."
-mkdir /var/www/html/vnstati
-echo "vnstati -s -i tun0 -o /var/www/html/vnstati/summary.png" >> ~/torrentbox/vnstati.sh
-echo "vnstati -h -i tun0 -o /var/www/html/vnstati/hourly.png" >> ~/torrentbox/vnstati.sh
-echo "vnstati -d -i tun0 -o /var/www/html/vnstati/daily.png" >> ~/torrentbox/vnstati.sh
-echo "vnstati -t -i tun0 -o /var/www/html/vnstati/top10.png" >> ~/torrentbox/vnstati.sh
-echo "vnstati -m -i tun0 -o /var/www/html/vnstati/monthly.png" >> ~/torrentbox/vnstati.sh
-chmod +x ~/torrentbox/vnstati.sh
-
+echo "setting up webinterace..."
+cp -r /home/pi/torrentbox-master/files/html /var/www/
 echo "setting up vnstat..."
 sed -i "s/Interface \"eth0\"/Interface \"tun0\"/" /etc/vnstat.conf
 rm -rf /lib/vnstat
 vnstat --create -i tun0
-
-echo "setting up cronjobs..."
-echo "0 */6 * * * sudo service openvpn restart >/dev/null 2>&1" >> cronjob
-echo "*/5 * * * * nice /home/pi/vnstati.sh >/dev/null 2>&1" >> cronjob
-crontab cronjob
-rm cronjob
-
-echo "set openvpn to autostart..."
-sed -i "s/#AUTOSTART=\"all\"/AUTOSTART=\"openvpn\"/" /etc/default/openvpn
-
-echo "setting up delay for qbittorrent..."
-sed -i "s/exit 0//" /etc/rc.local
-echo "sleep 7" >> /etc/rc.local #might need to be adapted depending on hdd speedup time
-echo "service qbittorrent start" >> /etc/rc.local
-echo "exit 0" >> /etc/rc.local
-
-echo "setting up motd..."
-echo "/home/pi/welcome.sh" >> /etc/profile
-echo "" >> /etc/motd
-#############something with init.d/motd need to verify process
-echo "MOTD will display your storage space, for this it needs to know how many devices you have attached. (Select 1 or 2)"
-while read -r -n 1 -s answer; do
-  if [[ $answer = [YyNn] ]]; then
-    [[ $answer = [Yy] ]] 
-    [[ $answer = [Nn] ]] && echo && echo -e "${RED}installation canceled :(" && exit 1 
-    break
-  fi
-done
-echo
-
-
-# sed template: sed -i "s/ ORIG/ REPLACEMENT /" PATH
+/etc/init.d/vnstat restart
 
 exit 0
+
--- files/installer.sh
+++ files/installer.sh
@@ -26,69 +26,59 @@ if ! [ $(id -u) = 0 ]; then
    exit 1
 fi
 echo -e "root ${RED}[OK]${DEF}"
-sleep 1
 
-#idk why, but in testing sometimes all files lose execute permission. shouldn't happen, but it does...
-#remove after testing on pi
-chmod 777 *sh
+# >/dev/null 2>&1
+# later put in
 
 #update+upgrade
 echo
 echo -e "${DEF}updating system..."
-apt-get update 2>&1 >> log.txt
+apt-get update
 echo -e "update ${RED}[DONE]${DEF}"
 echo
-sleep 1
 echo "upgrading system..."
-apt-get upgrade -y 2>&1 >> log.txt
+apt-get upgrade -y
 echo -e "upgrade ${RED}[DONE]${DEF}"
 echo
-sleep 1
 
 #install openvpn
 echo "installing openvpn..."
-apt-get install openvpn -y 2>&1 >> log.txt
+apt-get install openvpn -y
 echo -e "openvpn ${RED}[INSTALLED]${DEF}"
 echo
-sleep 1
 
 #install qbittorrent-nox
 echo "installing qbittorrent-nox..."
-apt-get install qbittorrent-nox -y 2>&1 >> log.txt
+apt-get install qbittorrent-nox -y
 echo -e "qbittorrent-nox ${RED}[INSTALLED]${DEF}"
 echo
-sleep 1
 
-#install fail2ban
-echo "installing fail2ban..."
-apt-get install fail2ban -y 2>&1 >> log.txt
+#install fail2ban and tree, just for a visual thing
+echo "installing tree fail2ban..."
+apt-get install fail2ban -y
 echo -e "fail2ban ${RED}[INSTALLED]${DEF}"
 echo
-sleep 1
 
 #install samba, 3 echo lines to surpress the popup
 echo "samba-common samba-common/workgroup string  WORKGROUP" | debconf-set-selections
 echo "samba-common samba-common/dhcp boolean true" | debconf-set-selections
 echo "samba-common samba-common/do_debconf boolean true" | debconf-set-selections
 echo "installing samba..."
-apt-get install samba samba-common-bin -y 2>&1 >> log.txt
+apt-get install samba samba-common-bin -y
 echo -e "samba ${RED}[INSTALLED]${DEF}"
 echo
-sleep 1
 
-#install samba
+#install vnstat
 echo "installing vnstat..."
-apt-get install vnstat vnstati -y 2>&1 >> log.txt
+apt-get install vnstat vnstati -y
 echo -e "vnstat ${RED}[INSTALLED]${DEF}"
 echo
-sleep 1
 
 #install lighttpd
 echo "installing lighttpd..."
-apt-get install lighttpd -y 2>&1 >> log.txt
+apt-get install lighttpd -y
 echo -e "lighttpd ${RED}[INSTALLED]${DEF}"
 echo
-sleep 1
 
 #install speedtest
 echo "Would you like to install speedtest-cli?"
@@ -96,24 +86,22 @@ echo "(speedtest-cli is a command line interface for testing internet
 bandwidth using speedtest.net) (Y/N)"
 while read -r -n 1 -s answer; do
   if [[ $answer = [YyNn] ]]; then
-    [[ $answer = [Yy] ]] && echo && echo "installing speedtest-cli..." && apt-get install speedtest-cli -y 2>&1 >> log.txt && echo -e "speedtest-cli ${RED}[INSTALLED]${DEF}"
+    [[ $answer = [Yy] ]] && echo && echo "installing speedtest-cli..." && apt-get install speedtest-cli -y >/dev/null 2>&1 && echo -e "speedtest-cli ${RED}[INSTALLED]${DEF}"
     [[ $answer = [Nn] ]] && echo && echo -e "${RED}not installing speedtest-cli${DEF}"
     break
   fi
 done
 echo
-sleep 1
 
 #install netdata?
 echo "Would you like to install netdata?"
 echo "(Netdata is a extremely optimized Linux utility that provides real-time (per second) performance monitoring for Linux systems, applications, SNMP devices, etc. and shows full interactive charts that absolutely render all collected values over the web browser to analyze them.) (Y/N)"
 while read -r -n 1 -s answer; do
   if [[ $answer = [YyNn] ]]; then
-    [[ $answer = [Yy] ]] && echo && echo "installing packages required for netdata..." && apt-get install zlib1g-dev uuid-dev libuv1-dev liblz4-dev libjudy-dev libssl-dev libmnl-dev gcc make git autoconf autoconf-archive autogen automake pkg-config curl python -y 2>&1 >> log.txt && echo -e "required packages ${RED}[INSTALLED]${DEF}" && echo && echo "installing netdata..." && echo "dev git clone" && git clone https://github.com/netdata/netdata.git --depth=100 -q && echo "dev cd" && cd netdata && echo "dev execute script" &&./netdata-installer.sh && echo && echo "DO NOT DELETE THE NETDATA FOLDER AFTER INSTALLATION, THIS WILL BREAK IT."
+    [[ $answer = [Yy] ]] && echo && echo "installing packages required for netdata..." && apt-get install zlib1g-dev uuid-dev libuv1-dev liblz4-dev libjudy-dev libssl-dev libmnl-dev gcc make git autoconf autoconf-archive autogen automake pkg-config curl python -y >/dev/null 2>&1 && echo -e "required packages ${RED}[INSTALLED]${DEF}" && echo && echo "installing netdata..." && echo "dev git clone" && git clone https://github.com/netdata/netdata.git --depth=100 -q && echo "dev cd" && cd netdata && echo "dev execute script" &&./netdata-installer.sh && echo && echo "DO NOT DELETE THE NETDATA FOLDER AFTER INSTALLATION, THIS WILL BREAK IT."
     [[ $answer = [Nn] ]] && echo && echo -e "${RED}not installing netdata${DEF}"
     break
   fi
 done
-sleep 1
 
 exit 0
