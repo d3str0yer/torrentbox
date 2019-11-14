de264e722cc659228769393591d2c7d2173d3c8f
d3str0yer <56647723+d3str0yer@users.noreply.github.com>
2019-11-14 13:30:34 +0000

    updated welcomex.sh, translated, devicenames

--- files/welcome2.sh
+++ files/welcome2.sh
@@ -1,53 +1,52 @@
 #!/bin/sh
-# dynamische MOTD
+# MOTD
 clear
 echo
-# Aufruf in /etc/profile (letzte Zeile)
-# Datum & Uhrzeit
-DATUM=`date +"%A, %e %B %Y"`
+# Date
+DATE=`date +"%A, %e %B %Y"`
 # Hostname
 HOSTNAME=`hostname -f`
-# letzter Login
-LAST1=`last -2 -a | awk 'NR==2{print $3}'`    # Wochentag
-LAST2=`last -2 -a | awk 'NR==2{print $5}'`    # Tag
-LAST3=`last -2 -a | awk 'NR==2{print $4}'`    # Monat
-LAST4=`last -2 -a | awk 'NR==2{print $6}'`    # Uhrzeit
-LAST5=`last -2 -a | awk 'NR==2{print $10}'`    # Remote-Computer
+# Last Login
+LAST1=`last -2 -a | awk 'NR==2{print $3}'`    # Weekday
+LAST2=`last -2 -a | awk 'NR==2{print $5}'`    # Day
+LAST3=`last -2 -a | awk 'NR==2{print $4}'`    # Month
+LAST4=`last -2 -a | awk 'NR==2{print $6}'`    # Time
+LAST5=`last -2 -a | awk 'NR==2{print $10}'`   # Remote Host
 # Uptime
 UP0=`cut -d. -f1 /proc/uptime`
-UP1=$(($UP0/86400))        # Tage
-UP2=$(($UP0/3600%24))        # Stunden
-UP3=$(($UP0/60%60))        # Minuten
-UP4=$(($UP0%60))        # Sekunden
-# Durchschnittliche Auslasung
-LOAD1=`cat /proc/loadavg | awk '{print $1}'`    # Letzte Minute
-LOAD2=`cat /proc/loadavg | awk '{print $2}'`    # Letzte 5 Minuten
-LOAD3=`cat /proc/loadavg | awk '{print $3}'`    # Letzte 15 Minuten
-# Temperatur
+UP1=$(($UP0/86400))     # Days
+UP2=$(($UP0/3600%24))   # Hours
+UP3=$(($UP0/60%60))     # Minutes
+UP4=$(($UP0%60))        # Seconds
+# Average Load
+LOAD1=`cat /proc/loadavg | awk '{print $1}'`    # Last Minute
+LOAD2=`cat /proc/loadavg | awk '{print $2}'`    # Last 5 Minutes
+LOAD3=`cat /proc/loadavg | awk '{print $3}'`    # Last 15 Minutes
+# Temperature
 TEMP=`vcgencmd measure_temp | cut -c "6-9"`
-# Speicherbelegung USB
-DISK1=`df -h | grep 'dev/sda' | awk '{print $2}'`    # Gesamtspeicher
-DISK2=`df -h | grep 'dev/sda' | awk '{print $3}'`    # Belegt
-DISK3=`df -h | grep 'dev/sda' | awk '{print $5}'`    # Belegt%
-DISK4=`df -h | grep 'dev/sda' | awk '{print $4}'`    # Frei
+# USB Disk Usage
+DISK1=`df -h | grep 'dev/sda1' | awk '{print $2}'`    # Total
+DISK2=`df -h | grep 'dev/sda1' | awk '{print $3}'`    # Used
+DISK3=`df -h | grep 'dev/sda1' | awk '{print $5}'`    # Used%
+DISK4=`df -h | grep 'dev/sda1' | awk '{print $4}'`    # Free
 # Speicherbelegung HDD
-DISK5=`df -h | grep 'dev/sdb1' | awk '{print $2}'`    # Gesamtspeicher
-DISK6=`df -h | grep 'dev/sdb1' | awk '{print $3}'`    # Belegt
-DISK7=`df -h | grep 'dev/sdb1' | awk '{print $5}'`    # Belegt%
-DISK8=`df -h | grep 'dev/sdb1' | awk '{print $4}'`    # Frei
+DISK5=`df -h | grep 'dev/sdb1' | awk '{print $2}'`    # Total
+DISK6=`df -h | grep 'dev/sdb1' | awk '{print $3}'`    # Used
+DISK7=`df -h | grep 'dev/sdb1' | awk '{print $5}'`    # Used%
+DISK8=`df -h | grep 'dev/sdb1' | awk '{print $4}'`    # Free
 # Arbeitsspeicher
 RAM1=`free -h --si | grep 'Mem' | awk '{print $2}'`    # Total
 RAM2=`free -h --si | grep 'Mem' | awk '{print $3}'`    # Used
 RAM3=`free -h --si | grep 'Mem' | awk '{print $4}'`    # Free
-RAM4=`free -h --si | grep 'Swap' | awk '{print $3}'`    # Swap used
-echo "\033[1;32m   .~~.   .~~.    \033[1;36m$DATUM
+RAM4=`free -h --si | grep 'Swap' | awk '{print $3}'`   # Swap used
+echo -e "\033[1;32m   .~~.   .~~.    \033[1;36m$DATE
 \033[1;32m  '. \ ' ' / .'   
 \033[1;31m   .~ .~~~..~.    \033[0;37mHostname......: \033[1;33m$HOSTNAME
-\033[1;31m  : .~.'~'.~. :   \033[0;37mLetzter Login.: $LAST1, $LAST2 $LAST3 $LAST4 von $LAST5
-\033[1;31m ~ (   ) (   ) ~  \033[0;37mUptime........: $UP1 Tage, $UP2 Stunden, $UP3 Minuten
-\033[1;31m( : '~'.~.'~' : ) \033[0;37mØ Auslastung..: $LOAD1 (1 Min.) | $LOAD2 (5 Min.) | $LOAD3 (15 Min.)
-\033[1;31m ~ .~ (   ) ~. ~  \033[0;37mTemperatur....: $TEMP °C
-\033[1;31m  (  : '~' :  )   \033[0;37mSpeicher USB..: Gesamt: $DISK1 | Belegt: $DISK2($DISK3) | Frei: $DISK4
-\033[1;31m   '~ .~~~. ~'    \033[0;37mSpeicher HDD..: Gesamt: $DISK5 | Belegt: $DISK6($DISK7) | Frei: $DISK8
-\033[1;31m       '~'        \033[0;37mRAM (MB)......: Gesamt: $RAM1 | Belegt: $RAM2 | Frei: $RAM3 | Swap: $RAM4
+\033[1;31m  : .~.'~'.~. :   \033[0;37mLast Login....: $LAST1, $LAST2 $LAST3 $LAST4 from $LAST5
+\033[1;31m ~ (   ) (   ) ~  \033[0;37mUptime........: $UP1 Days, $UP2 Hours, $UP3 Minutes
+\033[1;31m( : '~'.~.'~' : ) \033[0;37mØ Usage.......: $LOAD1 (1 min) | $LOAD2 (5 min) | $LOAD3 (15 min)
+\033[1;31m ~ .~ (   ) ~. ~  \033[0;37mTemperature...: $TEMP °C
+\033[1;31m  (  : '~' :  )   \033[0;37mStorage USB...: Total: $DISK1 | Used: $DISK2($DISK3) | Free: $DISK4
+\033[1;31m   '~ .~~~. ~'    \033[0;37mStorage HDD...: Total: $DISK5 | Used: $DISK6($DISK7) | Free: $DISK8
+\033[1;31m       '~'        \033[0;37mRAM (MB)......: Total: $RAM1 | Used: $RAM2 | Free: $RAM3 | Swap: $RAM4
 \033[m"
